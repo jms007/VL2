646a268a3390bb71502edb3356422113dd4a9b7c
JM Smith <quitomaster@gmail.com>
2018-02-15 12:24:20 -0600

    mod vl2 #3

--- src/com/extant/vl2/VL2.java
+++ src/com/extant/vl2/VL2.java
@@ -68,7 +68,7 @@ public class VL2
 
     public void VL2Init(String args[])
     {
-        System.out.println("Enter VL2Init");
+        System.out.println("Enter VL2Init mod #2");
         
 //        {   // Display Logo only on initial call
 //            try
