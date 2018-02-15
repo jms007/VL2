a077054fdf31d147a9a37609ad34991eeb2090f2
JM Smith <quitomaster@gmail.com>
2018-02-15 12:41:22 -0600

    repair

--- src/com/extant/vl2/VL2.java
+++ src/com/extant/vl2/VL2.java
@@ -68,7 +68,7 @@ public class VL2
 
     public void VL2Init(String args[])
     {
-        System.out.println("Enter VL2Init mod #5");
+        System.out.println("Enter VL2Init");
         
 //        {   // Display Logo only on initial call
 //            try
